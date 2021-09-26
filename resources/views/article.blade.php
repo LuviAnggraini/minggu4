9ba2a80219c52ea349dbd4e4fea0ee7c7753b7ed
LuviAnggraini <angelutvi@gmail.com>
2021-09-26 10:30:58 +0700

    MembuatPratikum1

---
+++ resources/views/article.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>article</title>
+    </head>
+    <body>
+        <h1>Halaman article</h1>
+        <h3>Menampilkan data article id ke-{{ $id }}</h3>
+        {{ $article }}
+    </body>
+</html>
